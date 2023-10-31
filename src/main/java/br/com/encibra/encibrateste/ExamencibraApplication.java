64674732cc50006658e9b36e088aeda0a9cbe0a3
Bruno fonseca <bruno.fonseca@amazonitec.com>
2023-10-31 00:41:44 -0300

    Inserindo no banco usuário e senha ao iniciar a aplicação.

--- src/main/java/br/com/encibra/encibrateste/ExamencibraApplication.java
+++ src/main/java/br/com/encibra/encibrateste/ExamencibraApplication.java
@@ -1,13 +1,35 @@
 package br.com.encibra.encibrateste;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ExamencibraApplication {
+import br.com.encibra.encibrateste.domain.Senha;
+import br.com.encibra.encibrateste.domain.Usuario;
+import br.com.encibra.encibrateste.repositories.SenhaRepository;
+import br.com.encibra.encibrateste.repositories.UsuarioRepository;
 
+@SpringBootApplication
+public class ExamencibraApplication implements CommandLineRunner {
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@Autowired
+	private SenhaRepository senhaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ExamencibraApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Usuario usuario = new Usuario(null, "Pessoa A");
+		usuarioRepository.save(usuario);
+		
+		Senha senha = new Senha(null, "Descrição A", "Tags A, Tags B, Tags C", "123456", usuario);
+		senhaRepository.save(senha);		
 	}
 
 }
