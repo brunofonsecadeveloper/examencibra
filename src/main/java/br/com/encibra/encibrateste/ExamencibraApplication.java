ece2f62d52702a5dd137c83a936eb883af8390e2
Bruno fonseca <bruno.fonseca@amazonitec.com>
2023-10-31 01:04:28 -0300

    Criptografando senha ao iniciar a aplicação

--- src/main/java/br/com/encibra/encibrateste/ExamencibraApplication.java
+++ src/main/java/br/com/encibra/encibrateste/ExamencibraApplication.java
@@ -1,6 +1,8 @@
 package br.com.encibra.encibrateste;
 
+import org.jasypt.util.text.BasicTextEncryptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,6 +21,9 @@ public class ExamencibraApplication implements CommandLineRunner {
 	@Autowired
 	private SenhaRepository senhaRepository;
 	
+	@Value("${jasypt.encryptor.password}")
+    private String secret;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ExamencibraApplication.class, args);
 	}
@@ -28,7 +33,9 @@ public class ExamencibraApplication implements CommandLineRunner {
 		Usuario usuario = new Usuario(null, "Pessoa A");
 		usuarioRepository.save(usuario);
 		
-		Senha senha = new Senha(null, "Descrição A", "Tags A, Tags B, Tags C", "123456", usuario);
+		BasicTextEncryptor encryptor = new BasicTextEncryptor();
+        encryptor.setPassword(secret);
+		Senha senha = new Senha(null, "Descrição A", "Tags A, Tags B, Tags C", encryptor.encrypt("123456"), usuario);
 		senhaRepository.save(senha);		
 	}
 
