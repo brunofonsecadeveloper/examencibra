f75a8b5dbb47bbd03ec400efad11ec169b8662ce
Bruno fonseca <bruno.fonseca@amazonitec.com>
2023-10-31 00:24:14 -0300

    Criação de projeto Spring.

---
+++ src/main/java/br/com/encibra/encibrateste/ExamencibraApplication.java
@@ -0,0 +1,13 @@
+package br.com.encibra.encibrateste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamencibraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamencibraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/encibra/encibrateste/ExamencibraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.encibra.encibrateste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamencibraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
