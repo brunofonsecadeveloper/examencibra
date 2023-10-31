991e7ddf9fa46f2d7279d36cb0a02b654b45ae63
Bruno fonseca <bruno.fonseca@amazonitec.com>
2023-10-31 02:36:30 -0300

    Adicionando ExceptionHandler para escutar e tratar exceções MethodArgumentNotValidException.

--- src/main/java/br/com/encibra/encibrateste/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/br/com/encibra/encibrateste/resources/exceptions/ResourceExceptionHandler.java
@@ -3,6 +3,8 @@ package br.com.encibra.encibrateste.resources.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -17,6 +19,16 @@ public class ResourceExceptionHandler {
 		
 		StandardError err = new StandardError(System.currentTimeMillis(), HttpStatus.FORBIDDEN.value(), "Limite de senhas cadastradas excedido.", e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(err);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+		
+		ValidationError err = new ValidationError(System.currentTimeMillis(), HttpStatus.UNPROCESSABLE_ENTITY.value(), "Erro de validação", e.getTitleMessageCode(), request.getRequestURI());
+		for (FieldError x : e.getBindingResult().getFieldErrors()) {
+			err.addError(x.getField(), x.getDefaultMessage());
+		}		
+		return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(err);
 	}
 	
 }
