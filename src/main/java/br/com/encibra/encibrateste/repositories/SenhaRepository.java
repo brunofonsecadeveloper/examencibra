15a3b6548a7843f6f003c456113a36ec020ba6d8
Bruno fonseca <bruno.fonseca@amazonitec.com>
2023-10-31 06:24:27 -0300

    Criando findByUsuario().

--- src/main/java/br/com/encibra/encibrateste/repositories/SenhaRepository.java
+++ src/main/java/br/com/encibra/encibrateste/repositories/SenhaRepository.java
@@ -1,11 +1,15 @@
 package br.com.encibra.encibrateste.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.encibra.encibrateste.domain.Senha;
+import br.com.encibra.encibrateste.domain.Usuario;
 
 @Repository
 public interface SenhaRepository extends JpaRepository<Senha, Integer> {
 
+	List<Senha> findByUsuario(Usuario usuario);
 }
