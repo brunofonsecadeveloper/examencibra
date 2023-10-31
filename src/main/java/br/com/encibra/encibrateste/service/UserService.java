6a4102145cafa12b89bc18f8efd4d108e0de8af8
Bruno fonseca <bruno.fonseca@amazonitec.com>
2023-10-31 06:55:17 -0300

    Adicionando função para ver se usuário está autenticado (se o token ainda é válido).

--- src/main/java/br/com/encibra/encibrateste/service/UserService.java
+++ src/main/java/br/com/encibra/encibrateste/service/UserService.java
@@ -3,6 +3,7 @@ package br.com.encibra.encibrateste.service;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import br.com.encibra.encibrateste.domain.Usuario;
+import br.com.encibra.encibrateste.service.exceptions.ObjectNotFoundException;
 
 public class UserService {
 	
@@ -13,5 +14,12 @@ public class UserService {
 		catch (Exception e) {
 			return null;
 		}
+	}
+	
+	public static void userIsAuthenticated(Usuario user) {
+		if(user == null) {
+			System.out.println("Criar exceção");
+			throw new ObjectNotFoundException("Usuário autenticado não encontrado.");
+		}		
 	}
 }
