6c4e42d643c400a1de862afb4e7948fffdc6a4c5
Bruno fonseca <bruno.fonseca@amazonitec.com>
2023-10-31 00:32:26 -0300

    Adicionando classes de domínio para Senha e Usuario.

---
+++ src/main/java/br/com/encibra/encibrateste/domain/Senha.java
@@ -0,0 +1,99 @@
+package br.com.encibra.encibrateste.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "senha")
+public class Senha implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	private String descricao;
+	private String tags;
+	private String valor;
+	
+	@ManyToOne
+	@JoinColumn(name="id_usuario")
+	private Usuario usuario;
+	
+	public Senha() {
+	}
+
+	public Senha(Integer id, String descricao, String tags, String valor, Usuario usuario) {
+		super();
+		this.id = id;
+		this.descricao = descricao;
+		this.tags = tags;
+		this.valor = valor;
+		this.usuario = usuario;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getTags() {
+		return tags;
+	}
+
+	public void setTags(String tags) {
+		this.tags = tags;
+	}
+
+	public String getValor() {
+		return valor;
+	}
+
+	public void setValor(String valor) {
+		this.valor = valor;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Senha other = (Senha) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
